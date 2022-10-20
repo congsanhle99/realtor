ab3979db2dfc91b08c0f1de5f5db654dee3aa84f
CongSanhLe <congsanhle1999@gmail.com>
2022-10-20 17:44:47 +0700

    [20102022] copy share link func

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,16 +1,19 @@
 import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+import { FaShare } from "react-icons/fa";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import SwiperCore, { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
+import "swiper/css/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Spinner from "../components/Spinner";
 import { db } from "../firebase";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
-import "swiper/css/bundle";
 
 function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [copyShareLink, setCopyShareLink] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   useEffect(() => {
     const fetchListing = async () => {
@@ -47,6 +50,15 @@ function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[10%] right-[2%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex items-center justify-center hover:scale-95 active:scale-105 transition duration-300"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          toast.success("Copied");
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
     </main>
   );
 }
